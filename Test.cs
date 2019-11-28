ff0fb508b59d247b926ab1bab9ecbac176b4965e
Anor Neto <anornetoo@gmail.com>
2019-11-28 16:21:39 -0300

    Add project files.

---
+++ Test.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace HashdexChallenge1
+{
+    public class Printer
+    {
+        public static void print(string text)
+        {
+            Console.WriteLine(text);
+        }
+    }
+}
